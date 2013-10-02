9870dcbb55096093945a7f315d0e856827c95461
pakko <pakko73@qq.com>
2013-10-03 00:49:17 +0800

    fix bug for seq2sparse

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,13 @@
 #mahout seqdirectory -i ClassFile -o news-seq
-mvector -i news-seq -o news-vectors -lnorm -nv -wt tfidf -x 0 -a com.chenlb.mmseg4j.analysis.MMSegAnalyzer
+mvector -i news-seq -o news-vectors -lnorm -nv -wt tfidf -a com.chenlb.mmseg4j.analysis.MMSegAnalyzer
 #mahout split -i news-vectors/tfidf-vectors --trainingOutput news-train-vectors --testOutput news-test-vectors --randomSelectionPct 40 --overwrite --sequenceFiles -xm sequential
 mahout trainnb -i news-vectors/tfidf-vectors -el -o model -li labelindex -ow -c
 mahout testnb -i news-vectors/tfidf-vectors -m model -l labelindex -ow -o news-testing -c
+
+mkmeans --input news-vectors/tfidf-vectors --numClusters 10 --maxIter 10 --output kmeans -t1 3 -t2 6
+
+mahout clusterdump -i kmeans/clusters-10-final --pointsDir kmeans/clusteredPoints -o result.txt
+
+mahout kmeans -i news-vectors/tfidf-vectors/ -c kmeans-centroids -cl -o kmeans-clusters -k 10 -ow -x 10 -dm org.apache.mahout.common.distance.CosineDistanceMeasure
+
+mahout clusterdump -d news-vectors/dictionary.file-0 -dt sequencefile -i kmeans-clusters/clusters-2-final -n 20 -b 100 -o cdump.txt -p kmeans-clusters/clusteredPoints/
